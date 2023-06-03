7fa252e1b82ba5a0464c694bbc3df78e43d0925f
toru2220 <toru2220@gmail.com>
2023-06-03 19:27:39 +0900

    じおじおじお

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import importlib
 from flask import Flask
 
 if __name__ == '__main__':
-    app_directory = 'apps'  # アプリファイルが格納されているディレクトリ
+    app_directory = 'apps'  # アプリファイルが格納されているサブディレクトリ
     app_files = []
 
-    # ディレクトリ配下のファイルを検索し、.py拡張子を持つファイルを取得
+    # サブディレクトリ内のファイルを検索し、.py拡張子を持つファイルを取得
     for file in os.listdir(app_directory):
-        if file.endswith('.py') and file != 'main.py':
+        if file.endswith('.py'):
             app_files.append(file[:-3])
 
     for app_file in app_files:
-        module = importlib.import_module(app_file)
+        module_name = f"{app_directory}.{app_file}"
+        module = importlib.import_module(module_name)
         app = getattr(module, 'app')
         port = getattr(module, 'port')
         app.run(port=port)
